0d8bda1af36e95e3f541927f229a57fc5bcdeb84
Frederic Weisbecker <fweisbec@gmail.com>
2014-12-08 01:40:37 +0100

    Diplay latvian word in firefox before adding it
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>

--- bb-add-latvian-word.py
+++ bb-add-latvian-word.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
-import os, sys
+import os, sys, optparse, subprocess
 
 import WiktionaryCookie
 from Wikicode import *
 from Page import *
 from Iterator import *
+
+parser = optparse.OptionParser()
+parser.add_option("-l", "--latvian", type = "string", dest = "latvian")
+parser.add_option("-f", "--french", type = "string", dest = "french")
+(options, args) = parser.parse_args()
 
 pattern = \
 u"""== {{langue|lv}} ==
@@ -17,23 +22,26 @@ u"""== {{langue|lv}} ==
 '''%s''' {{pron||lv}}
 # [[%s#fr|%s]]."""
 
-# ex: cat nom_commun_fr.dump | ./bb-dump-latvian-translation.py
 def main():
-	lv = sys.argv[1].decode("utf-8")
-	fr = sys.argv[2].decode("utf-8")
+	lv = options.latvian.decode("utf-8")
+	fr = options.french.decode("utf-8")
 	frp = FrenchNounPage.from_noun(lv)
 	if frp.valid():
 		print "Page already exist"
 		return
 
 	wikicode = pattern % (lv, fr, fr.title())
+	# Preview wikicode
 	print wikicode
+	# Check word on navigator
+	subprocess.call(["firefox", "-new-tab", "http://fr.wiktionary.org/wiki/%s" % fr.encode("utf-8")], shell = False)
+	subprocess.call(["firefox", "-new-tab", "http://en.wiktionary.org/wiki/%s" % lv.encode("utf-8")], shell = False)
 	accept = raw_input("Commit change? (Y/n) ")
 	if accept == "n":
 		return
 	elif not accept or accept in ("y", "Y"):
 		c = Creator(lv, wikicode)
-		c.commit(u"Creation de la page du mot letton %s en miroir à la traduction de %s" % (lv, fr))
+		c.commit(u"Création de la page du mot letton %s en miroir à la traduction de %s" % (lv, fr))
 	else:
 		print "?"	
 
